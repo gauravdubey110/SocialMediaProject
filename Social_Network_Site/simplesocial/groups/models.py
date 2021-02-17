a7c650e014e8b3b56e29bcc6145ab96888daab27
Gaurav Dubey <56021774+gauravdubey110@users.noreply.github.com>
2021-02-17 20:28:13 +0530

    Add files via upload

---
+++ Social_Network_Site/simplesocial/groups/models.py
@@ -0,0 +1,49 @@
+from django import template
+from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+# Create your models here.
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+register = template.Library()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name='memberships')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_groups')
+
+    def __str__(self):
+        # pylint: disable=E1101
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group', 'user')
